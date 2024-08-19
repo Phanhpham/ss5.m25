ee76bd8386c1e39d84e6226d0ebc9c3d6814ae95
phuonganh <pphuonganh815@gmail.com>
2024-08-19 14:15:15 +0700

    ss5

---
+++ src/app/home/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react';
+import { useRouter } from 'next/navigation'
+export default function page() {
+    const router=useRouter();
+    const loginUser=()=>{
+        router.push("auth/login");
+    }
+  return (
+    <div className='flex gap-9'>
+        trang home 
+        <button onClick={loginUser}>login</button>
+        <button>register</button>
+    </div>
+  )
+}
+
---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function notFound() {
+  return (
+    <div>not-found
+       <h1>trang ko ton tai</h1> 
+    </div>
+  )
+} 
